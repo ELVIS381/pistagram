18ff289d22d2c82bcb36d636eab709fe7b9e5785
devansh26-2 <devanshchopra77@gmail.com>
2022-12-23 00:20:06 +0530

    Changes in Login Page

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ require('dotenv').config();
 const port = process.env.PORT || 3000
 
 //Middlewares
+app.use(express.static(__dirname + '/public'));
+app.set('views', (__dirname + '/views'))
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname+'/public'));
-app.use(express.urlencoded({extended: true}));
 app.use(
     session({
       secret: `${process.env.SESSION_SECRET}`,
