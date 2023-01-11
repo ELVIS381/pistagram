560caac3c4a929ca0978e8d65760c9478ba5974b
devansh26-2 <76148533+devansh26-2@users.noreply.github.com>
2023-01-12 01:54:28 +0530

    Update notifs.js

--- routes/notifs.js
+++ routes/notifs.js
@@ -1,30 +1,28 @@
 const router = require('express').Router()
-const { isAuthorized, isVerified } = require('../config/authCheck')
-const Posts = require('../models/Posts')
 const Users = require('../models/Users')
+const Posts = require('../models/Posts')
+const {isAuthorized, isVerified} = require('../config/authCheck')
 
-router.get('/', isAuthorized, isVerified, (req,res)=>{ 
-    Users.findById(req.session.user._id)
+router.get('/', isAuthorized, isVerified,  (req,res)=>{
+    Users.findOne({email: req.session.user.email})
     .then(doc => {
-        if(doc){
-            let notifs = doc.notifs
-            let posts = []
-            if(notifs.length > 0){
-                for(let i = 0; i < notifs.length; i++){
-                    Posts.findById(notifs[i][2])
-                    .then(post => {
-                        posts.push(post.file)
-                        if(posts.length === notifs.length){
-                            res.render('notifs', {notifs,posts})
-                        }
-                    })
-                    .catch(err => console.log(err))
-                }
-            }else{
-                res.render('notifs', {notifs,posts})
+        let notifs = doc.notifs
+        let posts = []
+        if(notifs.length > 0){
+            notifs.forEach(notif => {
+                Posts.findById(notif[2])
+                .then(post => {
+                    posts.push(post.file)
+                    if(posts.length == notifs.length){
+                        res.render('notifs', {notifs,posts})
+                    }
+                }).catch(err => console.log(err))
             }
-
+            )
+        }else{
+            res.render('notifs', {notifs,posts})
         }
+        
     })
     .catch(err => console.log(err))
 })
