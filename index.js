856b0f9909a76cf981c2003129f0efc0171a9747
devansh26-2 <76148533+devansh26-2@users.noreply.github.com>
2023-01-12 02:26:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,52 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
-const notifsRoute = require('./routes/notifs');
 const profileRoute = require('./routes/profile');
-const app = express();
+const notifRoute = require('./routes/notifs');
 require('dotenv').config();
 
-const port = process.env.PORT || 3000
-
-//Middlewares
 app.use(express.static(__dirname + '/public'));
 app.set('views', (__dirname + '/views'))
 app.set('view engine', 'ejs');
 app.use(
     session({
-      secret: `${process.env.SESSION_SECRET}`,
-      cookie: {
-        maxAge: 60000 * 60 * 24,
-      },
-      saveUninitialized: false,
-      resave: false,
+        secret : 'yoyohoneysingh',
+        cookie : {
+            maxAge : 60000 * 60 * 24
+        },
+        saveUninitialized : true,
+        resave : false
     })
-  );
+)
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //db connection
-mongoose.connect(process.env.DB_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err=>console.log(err))
-
-
-//Non-Routes
-app.get('/', (req, res) => {
-    if(req.session.user){
-        res.redirect('/posts')
-    }else{
-        res.render('index')
-    }
-});
-app.get('/about', (req, res) => res.render('about'));
+mongoose.connect(process.env.DB_URI,{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
 
 //Routes
 app.use('/auth', authRoute);
 app.use('/posts', postRoute);
-app.use('/notifs', notifsRoute);
 app.use('/profile', profileRoute);
+app.use('/notifs', notifRoute);
 
-// Starting the app
-app.listen(port, ()=>{
-    console.log(`Server is running on  port ${port}`);
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about');
+})
+
+const PORT = process.env.PORT || 3000; // || = OR, && = AND
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on the port ${PORT}`);
 })
