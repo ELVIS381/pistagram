825f9d407c74f574a707714849981df5f2c8eb1f
devansh26-2 <76148533+devansh26-2@users.noreply.github.com>
2023-01-12 01:56:19 +0530

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,22 +1,14 @@
-const router = require('express').Router()
-const { isAuthorized, isVerified } = require('../config/authCheck')
-const Users = require('../models/Users')
+const router = require('express').Router();
+const {isAuthorized, isVerified} = require('../config/authCheck');
+const Users = require('../models/Users');
+const Posts = require('../models/Posts');
 
-router.get('/', isAuthorized, isVerified, (req,res)=>{ 
-    Users.findById(req.session.user)
-    .then((doc)=>{
-        if(doc){
-            let pfp = doc.pfp
-            let fullname = doc.name
-            let email = doc.email
-            let posts = doc.posts
-            let username = doc.username
-            let likedPosts = doc.likedPosts
-            res.render('profile', {fullname, pfp, email, posts, username, likedPosts})
-        }else{
-            console.log('no docs')
-        }
-    }).catch(err => console.log(err))
+router.get('/', isAuthorized, isVerified, (req,res)=>{
+    Users.findOne({email: req.session.user.email})
+    .then(doc => {
+        res.render('profile', {user: doc});
+    })
+    .catch(err => console.log(err));
 })
 
 module.exports = router
