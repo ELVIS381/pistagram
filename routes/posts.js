37b936e67be0a957b77ee25cae67c1d46ef91038
devansh26-2 <76148533+devansh26-2@users.noreply.github.com>
2023-01-12 01:54:56 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,121 +1,114 @@
-const router = require('express').Router()
-const http = require('http');
-const { isAuthorized, isVerified } = require('../config/authCheck')
-const Posts = require('../models/Posts')
-const Users = require('../models/Users')
+const router = require('express').Router();
+const {isAuthorized, isVerified} = require('../config/authCheck');
+const Posts = require('../models/Posts');
+const Users = require('../models/Users');
 
-router.get('/', isAuthorized, isVerified, (req,res)=>{
+
+router.get('/', isAuthorized, isVerified, (req, res) => {
     Posts.find()
-    .then(docs => {
-        Users.findById(req.session.user._id)
+    .then(posts => {
+        Users.findOne({email: req.session.user.email})
         .then(user => {
-            res.render('posts', {docs,user})
+            res.render('posts', {posts: posts, loggedInUser: user});
         })
         .catch(err => console.log(err))
-    }).catch(err => console.log(err))
+    })
+    .catch(err => console.log(err));
 })
 
-router.post('/newpost', isAuthorized, isVerified, (req,res) => {
-    let file = req.body.fileUrl
+router.post('/newpost', isAuthorized, isVerified, (req,res)=>{
+    const file = req.body.fileUrl
     const caption = req.body.caption
 
-    newPost = new Posts({
-         file: file,
-         caption: caption,
-         userpfp: req.session.user.pfp,
-         username: req.session.user.username,
+    const newPost = new Posts({
+        file: file,
+        caption: caption,
+        username: req.session.user.username,
+        userpfp: req.session.user.pfp,
     })
 
     newPost.save()
-    .then(() => {
-        Users.findById(req.session.user._id)
-        .then(doc =>{
+    .then(()=>{
+        Users.findOne({email: req.session.user.email})
+        .then(doc => {
             let posts = doc.posts
             posts.push(file)
             doc.markModified('posts')
             doc.save()
-            .then(()=>{
+            .then(() => {
                 res.redirect('/profile')
             })
             .catch(err => console.log(err))
-        }).catch(err => console.log(err))
-    }).catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 })
 
-router.get('/like', isAuthorized, isVerified, (req,res) => {
-    let post = req.query.id
-    let sender = req.query.sender
-    let senderpfp = req.query.senderpfp
-    let receiver = req.query.receiver
-    
-    Posts.findById(post)
+router.get('/like', isAuthorized, isVerified, (req,res) =>{
+    const {id,sender,senderpfp,receiver} = req.query
+    Posts.findById(id)
     .then(doc => {
-        // incrementing likes
         doc.likes++
-        doc.markModified('likes')
         doc.save()
         .then(()=>{
-            // sending notif
-            Users.findOne({username : receiver})
-            .then(doc => {
-                notifs = doc.notifs
-                notifs.push([sender,senderpfp,post])
-                doc.markModified('notifs')
-                doc.save()
+            Users.findOne({username: receiver})
+            .then(receiver => {
+                notifs = receiver.notifs
+                notifs.push([
+                    sender,
+                    senderpfp,
+                    id
+                ])
+                receiver.markModified('notifs')
+                receiver.save()
                 .then(()=>{
-                    // adding likedPost
-                    Users.findOne({username : sender})
-                    .then(doc => {
-                        likedPosts = doc.likedPosts
-                        likedPosts.push(post)
-                        doc.markModified('likedPosts')
-                        doc.save()
+                    Users.findOne({email: req.session.user.email})
+                    .then(sender => {
+                        likedPosts = sender.likedPosts
+                        likedPosts.push(id)
+                        sender.markModified('likedPosts')
+                        sender.save()
                         .then(()=>{
                             res.json({
                                 data: true
                             })
                         })
-                        .catch(err => {
-                            console.log(err)
+                        .catch(err => 
                             res.json({
                                 data: false
                             })
-                        })
+                                )
                     })
-                    .catch(err => {
-                        console.log(err)
+                    .catch(err => 
                         res.json({
                             data: false
                         })
-                    })
+                            )
                 })
-                .catch(err => {
-                    console.log(err)
-                    res.json({
+                .catch(err =>
+                     res.json({
                         data: false
-                    })
-                })
-            })
-            .catch(err => {
-                console.log(err)
+                        })
+                          )
+            }).catch(err => 
                 res.json({
                     data: false
                 })
-            })
+                    )
         })
-        .catch(err => {
-            console.log(err)
+        .catch(err => 
             res.json({
                 data: false
             })
-        })
+            )
     })
-    .catch(err => {
-        console.log(err)
+    .catch(err => 
         res.json({
             data: false
         })
-    })
+        )
+
 })
 
 module.exports = router
